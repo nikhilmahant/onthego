f87d543735a1b3489c325e620ae044b04a7f20ac
nikhilmahant <nsmahant@gmail.com>
2025-03-28 13:22:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-    alert("Thank you for your interest! Please call or email to book an appointment with Dr. Surendar Huddar.");
-}
-
-function toggleMenu() {
-    const navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("active");
-}
